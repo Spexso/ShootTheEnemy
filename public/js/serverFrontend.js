7e44f650f15f0e2e43974538534f9c7ef9e2b559
Spexso <ipekipek48@hotmail.com>
2023-12-02 15:04:07 +0300

    initial commit

---
+++ public/js/serverFrontend.js
@@ -0,0 +1,254 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+// Create socket object 
+const socket = io()
+
+const scoreEl = document.querySelector('#scoreEl')
+
+// Device screens pixel ratio assign if no ratio set it to 1 which is default value
+const resolutionRatio = window.devicePixelRatio || 1
+
+canvas.width = innerWidth * resolutionRatio
+canvas.height = innerHeight * resolutionRatio
+
+const x = canvas.width / 2
+const y = canvas.height / 2
+
+// const player = new Player(x, y, 10, 'white')
+const FrontendPlayers = {}
+
+// Refresh players on frontend
+socket.on('refreshPlayers', (appPlayers) => {
+
+  // Iterate through every player that comes from backend
+  for (const id in appPlayers){
+    
+    const backendPlayer = appPlayers[id];
+
+    // If a new player connected to frontend side create that player / Else 
+    if(!FrontendPlayers[id]) {
+
+      /** Passing construct arguments as single object so I can see what kind of variables Player has
+       * Applying hsl instead of generic color to manipulate color of each spawned player 
+       */
+      FrontendPlayers[id] = new Player({
+        x:backendPlayer.x,
+        y:backendPlayer.y,
+        radius: backendPlayer.radius,
+        color: backendPlayer.color
+      })
+    } else {
+      
+      // Change coordinates of player on frontend based on changed position of backend player
+      FrontendPlayers[id].x = backendPlayer.x
+      FrontendPlayers[id].y = backendPlayer.y
+
+      console.log("Location changed")
+    }
+  }
+
+  // Iterate through every player that comes from Frontend
+  for (const id in FrontendPlayers){
+    
+    // If Player from frontend does not exist on backend delete player from game
+    if(!appPlayers[id]){
+
+      delete FrontendPlayers[id];
+    }
+  }
+
+
+   //console.log(FrontendPlayers);
+  // DEBUG
+})
+
+/*
+* No need currently 
+
+const projectiles = []
+const enemies = []
+const particles = []
+*/
+
+
+/*
+* Function not needed since enemies will be other players
+
+function spawnEnemies() {
+  setInterval(() => {
+    const radius = Math.random() * (30 - 4) + 4
+
+    let x
+    let y
+
+    if (Math.random() < 0.5) {
+      x = Math.random() < 0.5 ? 0 - radius : canvas.width + radius
+      y = Math.random() * canvas.height
+    } else {
+      x = Math.random() * canvas.width
+      y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
+    }
+
+    const color = `hsl(${Math.random() * 360}, 50%, 50%)`
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x)
+
+    const velocity = {
+      x: Math.cos(angle),
+      y: Math.sin(angle)
+    }
+
+    enemies.push(new Enemy(x, y, radius, color, velocity))
+  }, 1000)
+}
+*/
+
+let animationId
+function animate() {
+  animationId = requestAnimationFrame(animate)
+  c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+  c.fillRect(0, 0, canvas.width, canvas.height)
+  
+  // Draw all players
+  for (const id in FrontendPlayers) {
+
+    const PlayerToDraw = FrontendPlayers[id];
+
+    PlayerToDraw.draw();
+  }
+/*
+* No need currently 
+
+  for (let index = particles.length - 1; index >= 0; index--) {
+    const particle = particles[index]
+
+    if (particle.alpha <= 0) {
+      particles.splice(index, 1)
+    } else {
+      particle.update()
+    }
+  }
+
+  for (let index = projectiles.length - 1; index >= 0; index--) {
+    const projectile = projectiles[index]
+
+    projectile.update()
+
+    // remove from edges of screen
+    if (
+      projectile.x - projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      projectiles.splice(index, 1)
+    }
+  }
+
+  for (let index = enemies.length - 1; index >= 0; index--) {
+    const enemy = enemies[index]
+
+    enemy.update()
+
+    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+    //end game
+    if (dist - enemy.radius - player.radius < 1) {
+      cancelAnimationFrame(animationId)
+    }
+
+    for (
+      let projectilesIndex = projectiles.length - 1;
+      projectilesIndex >= 0;
+      projectilesIndex--
+    ) {
+      const projectile = projectiles[projectilesIndex]
+
+      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+
+      // when projectiles touch enemy
+      if (dist - enemy.radius - projectile.radius < 1) {
+        // create explosions
+        for (let i = 0; i < enemy.radius * 2; i++) {
+          particles.push(
+            new Particle(
+              projectile.x,
+              projectile.y,
+              Math.random() * 2,
+              enemy.color,
+              {
+                x: (Math.random() - 0.5) * (Math.random() * 6),
+                y: (Math.random() - 0.5) * (Math.random() * 6)
+              }
+            )
+          )
+        }
+        // this is where we shrink our enemy
+        if (enemy.radius - 10 > 5) {
+          score += 100
+          scoreEl.innerHTML = score
+          gsap.to(enemy, {
+            radius: enemy.radius - 10
+          })
+          projectiles.splice(projectilesIndex, 1)
+        } else {
+          // remove enemy if they are too small
+          score += 150
+          scoreEl.innerHTML = score
+
+          enemies.splice(index, 1)
+          projectiles.splice(projectilesIndex, 1)
+        }
+      }
+    }
+  }
+  */
+
+}
+
+animate()
+// spawnEnemies()
+
+
+window.addEventListener('keydown', (event) => {
+
+  // Abort if frontend player is not initialized yet
+  if(!FrontendPlayers[socket.id]){
+    return
+  }
+    
+  // Based on input select movement action
+  switch(event.code){
+
+    case 'KeyW':
+      console.log('W key pressed')
+      // FrontendPlayers[socket.id].y -= 10
+      socket.emit('keydown', 'keyW')
+      break
+
+    case 'KeyA':
+      console.log('A key pressed')
+      // FrontendPlayers[socket.id].x -= 10
+      socket.emit('keydown', 'keyA')
+      break
+    
+    case 'KeyD':
+      console.log('D key pressed')  
+      // FrontendPlayers[socket.id].x += 10
+      socket.emit('keydown', 'keyD')
+      break
+      
+    case 'KeyS':
+      console.log('S key pressed')
+      // FrontendPlayers[socket.id].y += 10
+      socket.emit('keydown', 'keyS')
+      break
+  }
+
+  // const pressedKey = handleKeyEvent(event)
+
+  // DEBUG
+  console.log(event);
+  //console.log(pressedKey);
+})
---
+++ serverBackend.js
@@ -0,0 +1,110 @@
+// Create express server 
+// Later express server will be wrapped around http server
+const express = require('express')
+const app = express()
+
+// Set up http protocol 
+// Create the server from 'app'
+// 1-Since we are using socket.io, we need a http server to use it on socket.io 
+// 2-Thats why we wrap the http server around the express server  
+const http = require('http')
+const server = http.createServer(app)
+
+// Socket io setup 
+// Wrapped around http server 
+const { Server } = require('socket.io')
+const io = new Server(server, { pingInterval: 2000, pingTimeout: 3000})           // Checks clients in every 2 seconds & if no response in 3 seconds timeouts clients
+
+// Players array to store each player's informations
+const serverPlayers = {}
+
+// Amount of pixels to shift on movement
+const moveSpeed = 30
+
+const port = 3000
+
+app.use(express.static('public'))
+
+// Response to the requests
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  
+  console.log('A new player has connected');
+
+  // Create a new player with id comes from socket
+  // 2D Locations of players are randomized
+  serverPlayers[socket.id] = {
+    x: 600 * Math.random(),
+    y: 600 * Math.random(),
+    radius: 10,
+    color: `hsl(${360 * Math.random()}, 100%, 70%)`         // Generate random color for player 
+  }    
+  
+  // Broadcast new players to everyone
+  io.emit('refreshPlayers', serverPlayers)
+  
+  // If player disconnects delete that player and send a response
+  socket.on('disconnect', (reason) => {
+    
+    delete serverPlayers[socket.id]       // Player deletion
+    
+    console.log('A player has disconnected');
+    console.log(reason)
+    
+    io.emit('refreshPlayers', serverPlayers)
+  })
+
+  socket.on('keydown', (keycode) => {
+    console.log('Received keycode:', keycode);
+    //serverPlayers[socket.id].y -= 30
+
+    // Based on input select movement action
+    switch (keycode) {
+      case 'keyW':
+        console.log('Key W initiated');
+        serverPlayers[socket.id].y -= moveSpeed
+        break
+
+      case 'keyA':
+        serverPlayers[socket.id].x -= moveSpeed
+        break
+
+      case 'keyS':
+        serverPlayers[socket.id].y += moveSpeed
+        break
+
+      case 'keyD':
+        serverPlayers[socket.id].x += moveSpeed
+        break
+
+      default:
+        console.log("Key did not recognized")
+    }
+  })
+  
+  console.log(serverPlayers)
+})
+
+
+/**
+ * Server tick rate initialized here 
+ * Interval set as 15ms by default 
+ */
+setInterval( () => {
+
+  io.emit('refreshPlayers', serverPlayers)
+}, 15)
+
+
+
+
+// Listening on http server
+server.listen(port, () => {
+
+  console.log(`Program listening on port ${port}`);
+})
+
+console.log(`Server did load`);
