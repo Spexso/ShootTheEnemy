6944d60090d0bc8e4d126bb67f90ba9540c612f3
Kağan <89082422+Spexso@users.noreply.github.com>
2023-12-23 23:06:47 +0300

    Client-Side Projectiles
    Spawning projectiles from players position frontend only

--- public/js/eventListeners.js
+++ public/js/eventListeners.js
@@ -1,15 +1,30 @@
-/*
 addEventListener('click', (event) => {
+
+  const playerPosition = {
+    x: FrontendPlayers[socket.id].x,
+    y: FrontendPlayers[socket.id].y
+  }
+  // Get angle 
   const angle = Math.atan2(
-    event.clientY - canvas.height / 2,
-    event.clientX - canvas.width / 2
+    (event.clientY * window.devicePixelRatio) - playerPosition.y,
+    (event.clientX * window.devicePixelRatio) - playerPosition.x
   )
+
+  // Get velocity
   const velocity = {
     x: Math.cos(angle) * 5,
     y: Math.sin(angle) * 5
   }
-  projectiles.push(
-    new Projectile(canvas.width / 2, canvas.height / 2, 5, 'white', velocity)
+
+  frontEndProjectiles.push(
+    new Projectile({
+      x: playerPosition.x, 
+      y: playerPosition.y,
+      radius: 5,
+      color: 'white',
+      velocity
+    })
   )
+
+  console.log(frontEndProjectiles)
 })
-*/
--- public/js/serverFrontend.js
+++ public/js/serverFrontend.js
@@ -18,6 +18,9 @@ const y = canvas.height / 2
 // const player = new Player(x, y, 10, 'white')
 const FrontendPlayers = {}
 
+// Shot projectile array objects
+const frontEndProjectiles = []
+
 // Refresh players on frontend
 socket.on('refreshPlayers', (appPlayers) => {
 
@@ -82,7 +85,7 @@ socket.on('refreshPlayers', (appPlayers) => {
       delete FrontendPlayers[id];
     }
   }
-  
+
 })
 
 /*
@@ -139,93 +142,17 @@ function animate() {
 
     PlayerToDraw.draw();
   }
-/*
-* No need currently 
 
-  for (let index = particles.length - 1; index >= 0; index--) {
-    const particle = particles[index]
+ 
 
-    if (particle.alpha <= 0) {
-      particles.splice(index, 1)
-    } else {
-      particle.update()
-    }
+  for (let i = frontEndProjectiles.length - 1; i >= 0; i--) {
+
+    const frontEndProjectile = frontEndProjectiles[i]
+
+    frontEndProjectile.update()
+
   }
 
-  for (let index = projectiles.length - 1; index >= 0; index--) {
-    const projectile = projectiles[index]
-
-    projectile.update()
-
-    // remove from edges of screen
-    if (
-      projectile.x - projectile.radius < 0 ||
-      projectile.x - projectile.radius > canvas.width ||
-      projectile.y + projectile.radius < 0 ||
-      projectile.y - projectile.radius > canvas.height
-    ) {
-      projectiles.splice(index, 1)
-    }
-  }
-
-  for (let index = enemies.length - 1; index >= 0; index--) {
-    const enemy = enemies[index]
-
-    enemy.update()
-
-    const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
-
-    //end game
-    if (dist - enemy.radius - player.radius < 1) {
-      cancelAnimationFrame(animationId)
-    }
-
-    for (
-      let projectilesIndex = projectiles.length - 1;
-      projectilesIndex >= 0;
-      projectilesIndex--
-    ) {
-      const projectile = projectiles[projectilesIndex]
-
-      const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
-
-      // when projectiles touch enemy
-      if (dist - enemy.radius - projectile.radius < 1) {
-        // create explosions
-        for (let i = 0; i < enemy.radius * 2; i++) {
-          particles.push(
-            new Particle(
-              projectile.x,
-              projectile.y,
-              Math.random() * 2,
-              enemy.color,
-              {
-                x: (Math.random() - 0.5) * (Math.random() * 6),
-                y: (Math.random() - 0.5) * (Math.random() * 6)
-              }
-            )
-          )
-        }
-        // this is where we shrink our enemy
-        if (enemy.radius - 10 > 5) {
-          score += 100
-          scoreEl.innerHTML = score
-          gsap.to(enemy, {
-            radius: enemy.radius - 10
-          })
-          projectiles.splice(projectilesIndex, 1)
-        } else {
-          // remove enemy if they are too small
-          score += 150
-          scoreEl.innerHTML = score
-
-          enemies.splice(index, 1)
-          projectiles.splice(projectilesIndex, 1)
-        }
-      }
-    }
-  }
-  */
 
 }
 
