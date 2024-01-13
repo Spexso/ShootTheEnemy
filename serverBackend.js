e6521a075e2dfddd7ee22ccebf6864c58eade474
Kağan <89082422+Spexso@users.noreply.github.com>
2024-01-13 18:18:42 +0300

    Last Fix

--- serverBackend.js
+++ serverBackend.js
@@ -2,89 +2,76 @@
 // Later express server will be wrapped around http server
 const express = require('express')
 const app = express()
-
 // Set up http protocol 
 // Create the server from 'app'
 // 1-Since we are using socket.io, we need a http server to use it on socket.io 
 // 2-Thats why we wrap the http server around the express server  
 const http = require('http')
 const server = http.createServer(app)
-
 // Socket io setup 
 // Wrapped around http server 
 const { Server } = require('socket.io')
 const io = new Server(server, { pingInterval: 2000, pingTimeout: 3000})           // Checks clients in every 2 seconds & if no response in 3 seconds timeouts clients
-
 // Port Number for local run
 const port = 3000
-
 // Fixed Canvas Size
 const Constwidth = 1024
 const Constheight = 576
-
 app.use(express.static('public'))
-
 // Response to the requests
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
-
 /* Above Server Set up */
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 // Players array to store each player's informations
 const serverPlayers = {}
-
 // Amount of pixels to shift on movement
-const moveSpeed = 6
-
+const moveSpeed = 10
 // Radius of player pawn
 const RADIUS = 10
-
 // Projectiles array 
 const serverProjectiles = {}
 let projectileId = 0
 const PROJECTILE_RADIUS = 5
-
-
 // Player connection Handle
 // Function will stay alive until player disconnect
 // Event driven functions will be called inside this function 
 io.on('connection', (socket) => {
   
   console.log('A new player has connected');
-
+  
   
   // Broadcast new players to everyone
   io.emit('refreshPlayers', serverPlayers)
-
-
+  /**/
+  // Listen for connect then initialize canvas
+  socket.on('initCanvas', () => {
+    
+    
+  })
+  /**/
   /**/
   // Listen for shoot action 
   socket.on('shoot', ({x, y, angle}) => {
     projectileId++;
-
     // Calculate velocity
     const velocity = {
       x: Math.cos(angle) * 20,
       y: Math.sin(angle) * 20
     }
-
     serverProjectiles[projectileId] = {
       x,
       y,
       velocity,
       playerId: socket.id
     }
-
     console.log(serverProjectiles)
   })
   /**/
-
   /**/
   // Initialize defaults 
   socket.on('initGame', ({ username, width, height }) => {
-
     // Create a new player with id comes from socket
     // 2D Locations of players are randomized
     serverPlayers[socket.id] = {
@@ -96,16 +83,13 @@ io.on('connection', (socket) => {
       points: 0,
       username
     }    
-
     // Canvas initialization
     serverPlayers[socket.id].canvas = {
       width,
       height,
     }
-
   })
   /**/
-
   /**/
   // If player disconnects delete that player and send a response
   socket.on('disconnect', (reason) => {
@@ -118,99 +102,70 @@ io.on('connection', (socket) => {
     io.emit('refreshPlayers', serverPlayers)
   })
   /**/
-
   /**/
   // Player inputs will be caught here
   socket.on('keydown', ({keycode, sequenceNumber }) => {
     
     // Detection of canvas borders
     const backEndPlayer = serverPlayers[socket.id]
-    
-    // If player destroyed return
-    if(!serverPlayers[socket.id].sequenceNumber){
-      return
-    }
-    
+
+
     // Track sequence number of keys for individual player
     serverPlayers[socket.id].sequenceNumber = sequenceNumber
+    if(serverPlayers[socket.id].sequenceNumber){
+      serverPlayers[socket.id].sequenceNumber = sequenceNumber
+    }
+
 
     // Based on input select movement action
     switch (keycode) {
       case 'keyW':
-        //console.log('Key W initiated');
+        console.log('Key W initiated');
         serverPlayers[socket.id].y -= moveSpeed
         break
-
       case 'keyA':
-        //console.log('Key A initiated');
+        console.log('Key A initiated');
         serverPlayers[socket.id].x -= moveSpeed
         break
-
       case 'keyS':
-        //console.log('Key S initiated');
+        console.log('Key S initiated');
         serverPlayers[socket.id].y += moveSpeed
         break
-
       case 'keyD':
-        //console.log('Key D initiated');
+        console.log('Key D initiated');
         serverPlayers[socket.id].x += moveSpeed
         break
-
       default:
         console.log("Key did not recognized")
     }
-
-
     const playerSides = {
       left: backEndPlayer.x - backEndPlayer.radius,
-      right: backEndPlayer.x + backEndPlayer.radius,
-      top: backEndPlayer.y - backEndPlayer.radius,
-      bottom: backEndPlayer.y + backEndPlayer.radius
+      right: backEndPlayer.x + backEndPlayer.radius, 
+      top: backEndPlayer.y - backEndPlayer.radius, 
+      bottom: backEndPlayer.y + backEndPlayer.radius  
     }
-
-    if( playerSides.left < 0) {
+    if( playerSides.left < 0) 
       serverPlayers[socket.id].x = backEndPlayer.radius 
-      console.log("left")
-    }
-      
     
-    if( playerSides.right > 1024) {
+    if( playerSides.right > Constwidth)
       serverPlayers[socket.id].x = Constwidth - backEndPlayer.radius
-      console.log("right")
-    }
-      
-
-    if( playerSides.top < 0) {
+    if( playerSides.top < 0)
       serverPlayers[socket.id].y = backEndPlayer.radius
-      console.log("top")
-    }
-      
-
-    if( playerSides.bottom > 576){ 
+    if( playerSides.bottom > Constheight)
       serverPlayers[socket.id].y = Constheight - backEndPlayer.radius
-      console.log("bottom")
-    }
-      
   })
   /**/
-
   console.log(serverPlayers)
 })
-
-
 /**
  * Server tick rate initialized here 
  * Interval set as 15ms by default 
  */
 setInterval( () => {
-
   // Update projectiles positions
   for (const id in serverProjectiles) {
-
     serverProjectiles[id].x += serverProjectiles[id].velocity.x
     serverProjectiles[id].y += serverProjectiles[id].velocity.y
-
-
     
     // Delete projectile if it reaches end of canvas
     if(
@@ -224,11 +179,8 @@ setInterval( () => {
         delete serverProjectiles[id]
         continue;
       }
-
     for(const playerId in serverPlayers){
-
       const serverPlayer = serverPlayers[playerId]
-
       const distance_projectile_player = Math.hypot(
                                   serverProjectiles[id].x - serverPlayer.x, 
                                   serverProjectiles[id].y - serverPlayer.y)
@@ -237,9 +189,7 @@ setInterval( () => {
       if( 
         distance_projectile_player < PROJECTILE_RADIUS + serverPlayer.radius && 
         serverProjectiles[id].playerId !== playerId) {
-
           if(serverPlayers[serverProjectiles[id].playerId]){
-
             serverPlayers[serverProjectiles[id].playerId].points++
             console.log(serverPlayers[serverProjectiles[id].playerId])
           }
@@ -251,14 +201,10 @@ setInterval( () => {
       }
     }
   }
-
   io.emit('refreshProjectiles', serverProjectiles)
   io.emit('refreshPlayers', serverPlayers)
 }, 15)
-
-
 // Listening on http server
 server.listen(port, () => {
-
   console.log(`Program listening on port ${port}`);
 })
